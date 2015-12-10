e311162e23ba9328cd41bc596994d5abae9b515b
carl.duevel <carl.duevel@holisticon.de>
2015-12-10 12:43:12 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject de.otto/tesla-mongo-connect "0.1.8"
+(defproject de.otto/tesla-mongo-connect "0.1.9"
             :description "Addon to https://github.com/otto-de/tesla-microservice to read and write to mongodb."
             :url "https://github.com/otto-de/tesla-mongo-connect"
             :license {:name "Apache License 2.0"
                       :url  "http://www.apache.org/license/LICENSE-2.0.html"}
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-mongo-connect"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [com.novemberain/monger "3.0.1"]]
 
             :plugins [[lein-embongo "0.2.2"]]
@@ -16,7 +16,7 @@
                       :data-dir "./target/mongo-data-files"}
 
 
-            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.19"]]}}
+            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.24"]]}}
 
             :source-paths ["src"]
             :test-paths ["test" "test-resources"])
