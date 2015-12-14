268543866ebe6a1bed8cfea559769cb0059f76ac
Carl Düvel <carl.duevel@otto.de>
2015-12-14 19:33:34 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-mongo-connect "0.1.9"
+(defproject de.otto/tesla-mongo-connect "0.1.10"
             :description "Addon to https://github.com/otto-de/tesla-microservice to read and write to mongodb."
             :url "https://github.com/otto-de/tesla-mongo-connect"
             :license {:name "Apache License 2.0"
