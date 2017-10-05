4b20b726eb2a2ac8acd7455ded6f63f79c7afeb2
Carl Düvel <carl.duevel@otto.de>
2017-10-05 11:51:39 +0200

    lein-release plugin: bumped version from 0.3.3 to 0.3.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-mongo-connect "0.3.3"
+(defproject de.otto/tesla-mongo-connect "0.3.4-SNAPSHOT"
             :description "Addon to https://github.com/otto-de/tesla-microservice to read and write to mongodb."
             :url "https://github.com/otto-de/tesla-mongo-connect"
             :license {:name "Apache License 2.0"
